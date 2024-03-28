315213d3ada787739bbb05c20e5848da4ab52cbc
LucasSantosp09 <lucassantos.p09@gmail.com>
2024-03-28 20:56:30 -0300

    Criando QuestionsAnswerDTO, StudentCertificationAnswerDTO e StudentCertificationAnswer

---
+++ src/main/java/br/com/codepause/certification_nlw/modules/students/useCases/StudentCertificationAnswersUseCase.java
@@ -0,0 +1,18 @@
+package br.com.codepause.certification_nlw.modules.students.useCases;
+
+import br.com.codepause.certification_nlw.modules.students.dto.StudentCertificationAnswerDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentCertificationAnswersUseCase {
+
+public void execute(StudentCertificationAnswerDTO studentCertificationAnswerDTO){
+ //Verifica se usuário existe
+
+ //Buscar as alternativas das perguntas
+ //correta ou incorreta
+
+ //Salvar as informações da certificação
+}
+
+}
